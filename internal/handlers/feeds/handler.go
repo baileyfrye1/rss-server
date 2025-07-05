f90c4d5c47f0bcdc5fe936333d1442af7230c88a
Bailey Frye <baileyafrye@comcast.net>
2025-07-05 12:59:52 -0500

    Refactored functions and implemented channels to properly add data to response

--- internal/handlers/feeds/handler.go
+++ internal/handlers/feeds/handler.go
@@ -29,7 +29,7 @@ type Item struct {
 	Title   string `xml:"title"   json:"title"`
 	Link    string `xml:"link"    json:"link"`
 	PubDate string `xml:"pubDate" json:"pubDate"`
-	Content string `xml:"encoded" json:"encoded"`
+	Content string `xml:"encoded" json:"content"`
 }
 
 type SingularFeed struct {
@@ -46,36 +46,23 @@ func NewFeedHandler(l *log.Logger, urls []string) *Feeds {
 }
 
 func (f *Feeds) GetFeed(w http.ResponseWriter, r *http.Request) {
-	wg := sync.WaitGroup{}
+	var wg sync.WaitGroup
 	var response FeedsResponse
+	ch := make(chan SingularFeed, len(f.Urls))
 
 	wg.Add(len(f.Urls))
 	for _, url := range f.Urls {
-		go func() {
-			defer wg.Done()
-
-			rss := &RSS{}
-
-			data, err := fetchUrl(url, f.logger)
-			if err != nil {
-				f.logger.Println(err)
-				return
-			}
-
-			if err = xml.Unmarshal(data, rss); err != nil {
-				f.logger.Println(err)
-				return
-			}
-
-			cleanedFeed := stripHTMLTags(rss)
-
-			sf := SingularFeed{Source: rss.Channel.Title, Posts: cleanedFeed.Channel.Items}
-
-			response.Feeds = append(response.Feeds, sf)
-		}()
+		go f.processFeed(url, ch, &wg)
 	}
 
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for v := range ch {
+		response.Feeds = append(response.Feeds, v)
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(response)
@@ -85,10 +72,10 @@ func (f *Feeds) GetFeed(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func fetchUrl(url string, logger *log.Logger) ([]byte, error) {
+func (f *Feeds) fetchUrl(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		logger.Println(err)
+		f.logger.Println(err)
 		return nil, err
 	}
 
@@ -103,4 +90,25 @@ func stripHTMLTags(rss *RSS) *RSS {
 		rss.Channel.Items[i].Content = re.ReplaceAllString(r.Content, "")
 	}
 	return rss
+}
+
+func (f *Feeds) processFeed(url string, ch chan<- SingularFeed, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	rss := &RSS{}
+
+	data, err := f.fetchUrl(url)
+	if err != nil {
+		f.logger.Println(err)
+		return
+	}
+
+	if err = xml.Unmarshal(data, rss); err != nil {
+		f.logger.Println(err)
+		return
+	}
+
+	cleanedRss := stripHTMLTags(rss)
+
+	ch <- SingularFeed{Source: rss.Channel.Title, Posts: cleanedRss.Channel.Items}
 }
